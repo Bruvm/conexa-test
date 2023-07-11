912f0f80439e7e53a2044f52229e388aa1e8bb43
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2023-07-10 22:32:58 -0300

    character component, page and layout created

---
+++ src/character/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { AppBar, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <AppBar 
+    position='fix'
+    
+    >
+        <Typography>Hola</Typography>
+
+    </AppBar>
+  )
+}
---
+++ src/character/page/Character.jsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+import { CharacterLayout } from '../layout/CharacterLayout'
+
+export const Character = () => {
+  return (
+    <CharacterLayout>
+      <Typography> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid, distinctio. Aut beatae minus ipsum sed impedit quisquam inventore dolorum eaque ex suscipit atque aspernatur officiis, alias quos velit aliquam veritatis.</Typography>
+    </CharacterLayout>
+  )
+}
