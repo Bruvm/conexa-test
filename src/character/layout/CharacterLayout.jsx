51d957c419451919e58652cf733ad111a4ad9b02
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2023-07-13 19:53:48 -0300

    animate addeed

--- src/character/layout/CharacterLayout.jsx
+++ src/character/layout/CharacterLayout.jsx
@@ -10,27 +10,28 @@ const width = 100
 export const CharacterLayout = ({ children }) => {
 
     return (
-        <Box sx={{ display: 'flex' }}>
-            <Grid
-                container
-                spacing={2}
-                sx={{ minHeight: '100vh', backgroundColor: 'neutral.light', }}
-            >
 
-                <Header />
+        <Grid
+            container
+            spacing={2}
+            sx={{ minHeight: '100vh', backgroundColor: 'neutral.light', }}
+            className="animate__animated animate__fadeIn"
+        >
+
+            <Header />
 
 
-                <Grid item xs={12}>
-                    <Box
-                        component='main'
-                        sx={{ p: 3 }}
-                    >
-                        {children}
-                    </Box>
-                </Grid>
-
+            <Grid item xs={12} >
+                <Box
+                    component='main'
+                    sx={{p:3,marginTop: '50px'}}
+                >
+                    {children}
+                </Box>
             </Grid>
 
-        </Box>
+        </Grid>
+
+
     )
 }
